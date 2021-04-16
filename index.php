23e2d1d6d39f13fc50e52e12a3ed64de0dad89cb
dacaster <33090047+Neekast@users.noreply.github.com>
2021-04-16 10:55:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
-echo "Welcome to my PHP website";
+require 'includes/config.php';
+require "includes/function.php";
 
-echo "<br><br> ------------------------------ <br>";
-
-$n1=rand(1,100);
-
-echo $n1;
-
-?>
+init();
